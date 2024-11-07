2e4d7bbf394f9d3b566929806a7c470cbe713b7f
Milan Corluka <milan.corluka@levi9.com>
2024-11-07 18:06:22 +0100

    Second commit

--- Cafeteria/Menu.cs
+++ Cafeteria/Menu.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Channels;
-
-namespace Cafeteria
+﻿namespace Cafeteria
 {
     public class Menu
     {
--- Cafeteria/Order.cs
+++ Cafeteria/Order.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml.Linq;
-
-namespace Cafeteria
+﻿namespace Cafeteria
 {
     public class Order : Menu
     {
